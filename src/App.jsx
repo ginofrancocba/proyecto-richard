8e60f07107fefff2c2261b4af9dcaf41fde5969f
ginofrancocba <ginofrancocba@gmail.com>
2024-04-17 17:14:07 -0300

    Add Change Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -28,22 +28,3 @@ function App() {
 export default App;
 
 
-// function Navbar() {
-//     return (
-//         <div className="navbar">
-//             <ul>
-//                 <li><Link to="/">Home</Link></li>
-//                 <li><Link to="/pagina1">Página 1</Link></li>
-//                 <li><Link to="/pagina2">Página 2</Link></li>
-//                 <li><Link to="/pagina3">Página 3</Link></li>
-//                 <li><Link to="/pagina4">Página 4</Link></li>
-//                 <li><Link to="/pagina5">Página 5</Link></li>
-//                 <li><Link to="/pagina6">Página 6</Link></li>
-//                 <li><Link to="/pagina7">Página 7</Link></li>
-//                 <li><Link to="/pagina8">Página 8</Link></li>
-//             </ul>
-//         </div>
-//     );
-// }
-
-// export default Navbar;
