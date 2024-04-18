dff58f2bcf6896309cb9a6a0dd0719cb1cc7f8db
ginofrancocba <ginofrancocba@gmail.com>
2024-04-18 12:50:47 -0300

    Add Change main index App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './Home.jsx'; 
-import Pagina1 from './Pagina1.jsx';
-import Pagina2 from './Pagina2.jsx';
-import Pagina3 from './Pagina3.jsx';
-import Pagina4 from './Pagina4.jsx';
-import Pagina5 from './Pagina5.jsx';
-import Pagina6 from './Pagina6.jsx';
-import Pagina7 from './Pagina7.jsx';
-import Pagina8 from './Pagina8.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Pagina1 from './Pagina1';
+import Pagina2 from './Pagina2';
+import Pagina3 from './Pagina3';
+import Pagina4 from './Pagina4';
+import Pagina5 from './Pagina5';
+import Pagina6 from './Pagina6';
+import Pagina7 from './Pagina7';
+import Pagina8 from './Pagina8';
+import './index.css';
 
-
-function App() {
+const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} /> 
-      <Route path="/pagina1" component={Pagina1} /> 
-      <Route path="/pagina2" component={Pagina2} /> 
-      <Route path="/pagina3" component={Pagina3} /> 
-      <Route path="/pagina4" component={Pagina4} /> 
-      <Route path="/pagina5" component={Pagina5} /> 
-      <Route path="/pagina6" component={Pagina6} /> 
-      <Route path="/pagina7" component={Pagina7} /> 
-      <Route path="/pagina8" component={Pagina8} /> 
-    </Switch>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pagina1" element={<Pagina1 />} />
+            <Route path="/pagina2" element={<Pagina2 />} />
+            <Route path="/pagina3" element={<Pagina3 />} />
+            <Route path="/pagina4" element={<Pagina4 />} />
+            <Route path="/pagina5" element={<Pagina5 />} />
+            <Route path="/pagina6" element={<Pagina6 />} />
+            <Route path="/pagina7" element={<Pagina7 />} />
+            <Route path="/pagina8" element={<Pagina8 />} />
+          </Routes>
+        </div>
+    
+
   );
-}
+};
 
 export default App;
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import App from './App'; 
 import Navbar from './Navbar';
 import './index.css';
 
-
-ReactDOM.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
     <Router>
       <div>
         <Navbar />
         <App />
       </div>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
 );
