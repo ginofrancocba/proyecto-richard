9d284511eba4af2b5bf5162ad785810c3a3a8a96
ginofrancocba <ginofrancocba@gmail.com>
2024-04-19 13:14:38 -0300

    Add img HOME.JSX

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,37 @@ import React from 'react';
 
 const Home = () => {
       return (
-            
-        <h1>ESTO ES HOME...</h1>
+            <div>
+              <section>
+                <img class="img-header" src="public/img-header2.png" alt="image-richard" ></img>
+              </section>
+              <section>
+                <div className='p-home'>
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, culpa animi, aliquamñl kglghñrtjnh gñojtnrh gñjesn vo</p>
+                </div>
+                <div className='form'>               
+                </div>
+              </section>
+              <section>
+                <div>
+                  <h1>Próximos cursos</h1>
+                </div>
+                <div>
+                  <img class="img-curso" src="public/img-curso.jpeg" alt="image-curso" ></img>   
+                  <img class="img-libro" src="public/img-libro.jpg" alt="image-libro" ></img>
+                </div>
+              </section>
+              <section>
+                <div className='p-home'>
+                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, culpa animi, aliquamñl kglghñrtjnh gñojtnrh gñjesn vo</p>
+                  <img class="img-home1" src="public/img-home1.jpeg" alt="image-richard" ></img>  
+                </div>
+                <div> 
+                             
+                </div>
+              </section>
+            </div>
+     
       );
   };
   
